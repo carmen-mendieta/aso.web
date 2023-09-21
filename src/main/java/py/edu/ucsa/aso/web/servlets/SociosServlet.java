9cbc7679e46b9c24d6b97d9f58c2e7648514b19a
carmen-mendieta <135380157+carmen-mendieta@users.noreply.github.com>
2023-09-20 21:37:22 -0400

    Cambios Servlet

--- src/main/java/py/edu/ucsa/aso/web/servlets/SociosServlet.java
+++ src/main/java/py/edu/ucsa/aso/web/servlets/SociosServlet.java
@@ -1,13 +1,21 @@
 package py.edu.ucsa.aso.web.servlets;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Objects;
+import py.edu.ucsa.aso.web.jdbc.dao.DAOFactory;
+import py.edu.ucsa.aso.web.jdbc.dao.SocioDAO;
+import py.edu.ucsa.aso.web.jdbc.dto.Opcion;
+import py.edu.ucsa.aso.web.jdbc.dto.Socio;
+import py.edu.ucsa.aso.web.jdbc.dto.Usuario;
 
 /**
  * Servlet implementation class SociosServlet
@@ -15,78 +23,107 @@ import java.util.Objects;
 @WebServlet("/socio-servlet")
 public class SociosServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SociosServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public SociosServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		PrintWriter pw= response.getWriter();
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		SocioDAO socioDAO = DAOFactory.getSocioDAO();
+		Socio dto = new Socio();
+
+		if (Objects.nonNull(request.getParameter("apellidos"))) {
+			dto.setApellidos(request.getParameter("apellidos"));
+		}
+
+		if (Objects.nonNull(request.getParameter("nombres"))) {
+			dto.setNombres(request.getParameter("nombres"));
+		}
+
+		if (Objects.nonNull(request.getParameter("estadoActual"))) {
+			Opcion estado = new Opcion();
+			estado.setId(Integer.parseInt(request.getParameter("estadoActual")));
+			dto.setEstadoActual(estado);
+		}
+
+		LocalDateTime ahora = LocalDateTime.now();
+		dto.setFechaCreacion(ahora);
+		dto.setFechaEstadoActual(ahora);
+		dto.setFechaIngreso(ahora);
+
+		if (Objects.nonNull(request.getParameter("fundador")) && "on".equals(request.getParameter("fundador"))) {
+			dto.setFundador(true);
+
+		} else {
+			dto.setFundador(false);
+		}
+		if (Objects.nonNull(request.getParameter("email"))) {
+			dto.setEmail(request.getParameter("email"));
+		}
+
+		if (Objects.nonNull(request.getParameter("nroCedula"))) {
+			dto.setNroCedula(Integer.parseInt(request.getParameter("nroCedula")));
+		}
+
+		if (Objects.nonNull(request.getParameter("nroSocio"))) {
+			dto.setNroSocio(Integer.parseInt(request.getParameter("nroSocio")));
+		}
+
+		if (Objects.nonNull(request.getParameter("tipoSocio"))) {
+			Opcion tipoSocio = new Opcion();
+			tipoSocio.setId(Integer.parseInt(request.getParameter("tipoSocio")));
+			dto.setTipoSocio(tipoSocio);
+		}
+
+		dto.setUsuarioCreacion(new Usuario(1));
+		socioDAO.insertar(dto);
+
+		PrintWriter pw = response.getWriter();
 		pw.print("<html><head><title>Mostrar Parametros</title></head>");
 		pw.print("<body><h1>Parametro</h1><hr>");
-		if(Objects.nonNull(request.getParameter("nombres"))) {
-			pw.print("<br>Nombres: " +request.getParameter("nombres"));
-		}
-		if(Objects.nonNull(request.getParameter("apellidos"))) {
-			pw.print("<br>Apellidos: " +request.getParameter("apellidos"));
-		}
+
 		
-		if(Objects.nonNull(request.getParameter("email"))) {
-			pw.print("<br>Email: " +request.getParameter("email"));
-		}
 		
-		if(Objects.nonNull(request.getParameter("nroSocio"))) {
-			pw.print("<br>Numero de Socio: " +request.getParameter("nroSocio"));
-		}
-		
-		if(Objects.nonNull(request.getParameter("nroCedula"))) {
-			pw.print("<br>Numero de Cedula: " +request.getParameter("nroCedula"));
-		}
-		
-		if(Objects.nonNull(request.getParameter("fechaIngreso"))) {
-			pw.print("<br>Fecha de Ingreso: " +request.getParameter("fechaIngreso"));
-		}
-		
-		if(Objects.nonNull(request.getParameter("estadoActual"))) {
-			pw.print("<br>Estado: " +request.getParameter("estadoActual"));
-		}
-		
-		if(Objects.nonNull(request.getParameter("fundador"))) {
-			pw.print("<br> Es fundador: " +request.getParameter("fundador"));
-		}
-		
-		if(Objects.nonNull(request.getParameter("usuarioCreacion"))) {
-			pw.print("<br>Usuario Creacion: " +request.getParameter("usuarioCreacion"));
-		}
-		
-		if(Objects.nonNull(request.getParameter("fechaCreacion"))) {
-			pw.print("<br>Fecha de Creacion: " +request.getParameter("fechaCreacion"));
-		}
-		
-		if(Objects.nonNull(request.getParameter("socioProponente"))) {
-			pw.print("<br>Socio Proponente: " +request.getParameter("socioProponente"));
-		}
-		
-		if(Objects.nonNull(request.getParameter("tipoSocio"))) {
-			pw.print("<br>Tipo Socio: " +request.getParameter("tipoSocio"));
-		}
-				
+
+		/*
+		 * if (Objects.nonNull(request.getParameter("fechaIngreso"))) {
+		 * pw.print("<br>Fecha de Ingreso: " + request.getParameter("fechaIngreso")); }
+		 * 
+		 * if (Objects.nonNull(request.getParameter("usuarioCreacion"))) {
+		 * pw.print("<br>Usuario Creacion: " + request.getParameter("usuarioCreacion"));
+		 * }
+		 * 
+		 * if (Objects.nonNull(request.getParameter("fechaCreacion"))) {
+		 * pw.print("<br>Fecha de Creacion: " + request.getParameter("fechaCreacion"));
+		 * }
+		 * 
+		 * if (Objects.nonNull(request.getParameter("socioProponente"))) {
+		 * pw.print("<br>Socio Proponente: " + request.getParameter("socioProponente"));
+		 * }
+		 * 
+		 * if (Objects.nonNull(request.getParameter("tipoSocio"))) {
+		 * pw.print("<br>Tipo Socio: " + request.getParameter("tipoSocio")); }
+		 */
+
 	}
 
 }
