d4a07dfea6d7bc77e2de63f5222c038c9eec5139
carmen-mendieta <135380157+carmen-mendieta@users.noreply.github.com>
2023-11-25 11:52:25 -0300

    suspension-validacion

--- src/main/java/py/edu/ucsa/aso/web/servlets/SuspensionServlet.java
+++ src/main/java/py/edu/ucsa/aso/web/servlets/SuspensionServlet.java
@@ -21,75 +21,83 @@ import java.util.Objects;
 @WebServlet("/suspension-servlet")
 public class SuspensionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SuspensionServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		 if(Objects.isNull(request.getParameter("ACCION")) 
-				  || "".equals(request.getParameter("ACCION"))){
-			 
-			  if(Objects.isNull(request.getParameter("FORMATO"))
-					  || "HTML".equals(request.getParameter("FORMATO"))){ 
-				  request.getRequestDispatcher("suspension-socio.jsp").forward(request, response);
-			  } else if("JSON".equals(request.getParameter("FORMATO"))) {
-				  response.setContentType("application/json");
-				  if(Objects.isNull(request.getParameter("FILTRO"))){
-					  List<Socio> sociosSuspensiones = DAOFactory.getSocioDAO().getListadoSocios("TODOS");
-					  System.out.println("maria: " + sociosSuspensiones.size());
-					  response.getWriter().print(JSONArray.fromObject(sociosSuspensiones));
-				  }else {
-					  response.getWriter().print(JSONArray.fromObject(DAOFactory.getSocioDAO().getListadoSocios(request.getParameter("FILTRO"))));
-				  }
-			  }
-		 }
-		
-		
-		 
+	public SuspensionServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// System.out.println("parametro:"+request.getParameter("ID"));
-		
-			int idSocio = 0;
-			String observacion = "";
-			int idUsuarioConectado = 0;
-			HttpSession session = request.getSession(false);
-			
-			if("SUSPENDER".equals(request.getParameter("ACCION"))) {  
-				if (Objects.nonNull(request.getParameter("ID"))) {
-					idSocio = Integer.parseInt(request.getParameter("ID"));
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		if (Objects.isNull(request.getParameter("ACCION")) || "".equals(request.getParameter("ACCION"))) {
+
+			if (Objects.isNull(request.getParameter("FORMATO")) || "HTML".equals(request.getParameter("FORMATO"))) {
+				request.getRequestDispatcher("suspension-socio.jsp").forward(request, response);
+			} else if ("JSON".equals(request.getParameter("FORMATO"))) {
+				response.setContentType("application/json");
+				if (Objects.isNull(request.getParameter("FILTRO"))) {
+					List<Socio> sociosSuspensiones = DAOFactory.getSocioDAO().getListadoSocios("TODOS");
+					System.out.println("maria: " + sociosSuspensiones.size());
+					response.getWriter().print(JSONArray.fromObject(sociosSuspensiones));
+				} else {
+					response.getWriter().print(JSONArray
+							.fromObject(DAOFactory.getSocioDAO().getListadoSocios(request.getParameter("FILTRO"))));
 				}
-				
-				if (Objects.nonNull(request.getParameter("OBSERVACIONINPUT"))) {
-					observacion=request.getParameter("OBSERVACIONINPUT");
-				}
-				
-			    if (session != null) {
-			        Usuario usuarioConectado = (Usuario) session.getAttribute("USUARIO_CONECTADO");
-			        if (usuarioConectado != null) {
-			            idUsuarioConectado = usuarioConectado.getId();
-			            System.out.println("IdUsuario: " + idUsuarioConectado);
-			        }    
-			    }
-				
-			    DAOFactory.getSocioDAO().suspenderSocio(idSocio, observacion, idUsuarioConectado);
-			    request.getSession().setAttribute("TODOS", DAOFactory.getSocioDAO().getListadoSocios("TODOS"));
-			    request.getRequestDispatcher("suspension-socio.jsp").forward(request, response);
-		
 			}
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// System.out.println("parametro:"+request.getParameter("ID"));
+
+		int idSocio = 0;
+		String observacion = "";
+		int idUsuarioConectado = 0;
+		HttpSession session = request.getSession(false);
+
+		if ("SUSPENDER".equals(request.getParameter("ACCION"))) {
+			if (Objects.nonNull(request.getParameter("ID"))) {
+				idSocio = Integer.parseInt(request.getParameter("ID"));
+			}
+
+			if (Objects.nonNull(request.getParameter("OBSERVACIONINPUT"))) {
+				observacion = request.getParameter("OBSERVACIONINPUT");
+			}
+
+			if (session != null) {
+				Usuario usuarioConectado = (Usuario) session.getAttribute("USUARIO_CONECTADO");
+				if (usuarioConectado != null) {
+					idUsuarioConectado = usuarioConectado.getId();
+					System.out.println("IdUsuario: " + idUsuarioConectado);
+				}
+			}
+			try {
+				if(idSocio>0 && !observacion.isEmpty() && idUsuarioConectado > 0) {
+				DAOFactory.getSocioDAO().suspenderSocio(idSocio, observacion, idUsuarioConectado);
+				request.getSession().setAttribute("TODOS", DAOFactory.getSocioDAO().getListadoSocios("TODOS"));
+				request.getRequestDispatcher("suspension-socio.jsp").forward(request, response);
+				}else {
+					System.out.println("Alguna condicion no se cumple");
+					
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
